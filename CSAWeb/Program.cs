d9b1fa9f39048daade131cdefca4aa8da9957a05
Alec <alec.m.hancock@gmail.com>
2023-07-13 15:48:22 -0500

    Working build init

--- CSAWeb/Program.cs
+++ CSAWeb/Program.cs
@@ -1,17 +1,16 @@
-using CSAWeb.Data;
+using CSAWeb;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddMudServices();
+builder.Logging.AddConsole();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -20,12 +19,8 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
